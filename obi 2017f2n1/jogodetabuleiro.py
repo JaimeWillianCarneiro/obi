773bf513f0ec1666e621f7e2c44e4691915f9fe4
jaime willian <jwcs2@aluno.ifal.edu.br>
2023-07-26 17:08:39 -0300

    sonho bom

--- obi 2017f2n1/jogodetabuleiro.py
+++ obi 2017f2n1/jogodetabuleiro.py
@@ -1,9 +1,9 @@
 ordem = int(input())
 n = ordem
-linhas = []
-for x in range(n):
-    linha = list(map(int, input().split()))
-    linhas.append(linha)
+
+linhas = [list(map(int, input().split())) for _ in range(n)]
+
+
 
 for i in range(1, n):
     for j in range(1, n):
