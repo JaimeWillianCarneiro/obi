1861d7867e2ebec2d5816a88836f491d7c3a9b70
jaime <jwcs2@aluno.ifal.edu.br>
2023-07-30 10:29:58 -0300

    atualiza

--- obi 2017 f2n2/frete.py
+++ obi 2017 f2n2/frete.py
@@ -9,13 +9,13 @@ def dijkstra(grafo, origem, destino):
 
     distancias[origem] = 0
     prioridade = [(0, origem)]
-    print(prioridade)
+    # print(prioridade)
     while prioridade:
 
 
         atual_dist, atual_vertice = heapq.heappop(prioridade) #  Remove o elemento de menor distância (o primeiro elemento do heap) 
                                                                 #da fila de prioridade e atribui o valor da distância e do
-        print(prioridade)                                                                #  vértice a atual_dist e atual_vertice, respectivamente.
+        # print(prioridade)                                                                #  vértice a atual_dist e atual_vertice, respectivamente.
         
         if atual_dist > distancias[atual_vertice]:
             continue
@@ -36,20 +36,38 @@ def dijkstra(grafo, origem, destino):
     return float('inf')
 
 # Exemplo de uso:
-grafo = {
-    'A': {'B': 4, 'C': 2},
-    'B': {'A': 4, 'C': 5, 'D': 10},
-    'C': {'A': 2, 'B': 5, 'D': 3, 'E': 8},
-    'D': {'B': 10, 'C': 3, 'E': 7},
-    'E': {'C': 8, 'D': 7}
-}
+# grafo = {
+#     'A': {'B': 4, 'C': 2},
+#     'B': {'A': 4, 'C': 5, 'D': 10},
+#     'C': {'A': 2, 'B': 5, 'D': 3, 'E': 8},
+#     'D': {'B': 10, 'C': 3, 'E': 7},
+#     'E': {'C': 8, 'D': 7}
+# }
 
 # Encontre o menor caminho entre os vértices 'A' e 'D'
-origem = 'A'
-destino = 'D'
-menor_caminho = dijkstra(grafo, origem, destino)
 
+
+
+n, m = map(int, input().split())
+origem = 1
+destino = n
+
+
+ 
+grafo = {int(i): {} for i in range(1, n+1) }
+for _  in range(m):
+    a,b, c= map(int, input().split())
+    grafo[a][b] = c
+    grafo[b][a] = a
+
+
+
+menor_caminho = dijkstra(grafo, origem, destino)
 if menor_caminho != float('inf'):
     print(f"Menor caminho entre '{origem}' e '{destino}' é: {menor_caminho}")
 else:
     print(f"Não há caminho entre '{origem}' e '{destino}' no grafo.")
+
+
+
+
