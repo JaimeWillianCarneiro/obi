7dbe31bf8789d437aa7d233b0526f62687cc8709
jaime <jwcs2@aluno.ifal.edu.br>
2023-08-13 17:58:43 -0300

    nossa

---
+++ oboi/fortedemais.py
@@ -0,0 +1,44 @@
+e, d = map(int, input().split())
+
+
+esquerda = [int(i) for i in input().split()]
+direita = [int(i) for i in input().split()]
+
+
+l = e -1
+r = 0
+
+
+anilhas = 0 
+somaEsquerda = esquerda[-1]
+somaDireita = direita[0]
+
+
+while True:
+    if somaEsquerda== somaDireita:
+    
+
+        anilhas = l+1 + (d-(r+1))-1
+        break
+    else:
+    
+        if esquerda[l] < direita[r]:
+            l-=1
+            somaEsquerda+= esquerda[l]
+            
+        else:
+            r+=1
+            somaDireita+= direita[r]
+            
+
+
+print(anilhas)
+
+
+"""
+3 3
+1 7 1
+8 4 1
+"""
+            
+
