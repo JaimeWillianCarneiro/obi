9f2be7317d64c45d4d3dfa746417740984497a3e
Jaime Willian <104590276+JaimeWillianCarneiro@users.noreply.github.com>
2023-09-11 14:11:01 -0300

    Update fortedemaisia.py

--- oboi/fortedemaisia.py
+++ oboi/fortedemaisia.py
@@ -1,18 +1,19 @@
+# Recebendo as entradas
 e, d = map(int, input().split())
 
 esquerda = [int(i) for i in input().split()]
 direita = [int(i) for i in input().split()]
 
-l = 0
-r = d-1
+l = 0 # ponteiro representando a anilha da extremidade esquerda
+r = d-1 # ponteiro representando a anilha da extremidade direita
 
-anilhas = 0
-somaEsquerda = sum(esquerda)  # Alteração 1: Calcular a soma de esquerda
-somaDireita = sum(direita)    # Alteração 2: Calcular a soma de direita
+anilhasRetiradas = 0
+somaEsquerda = sum(esquerda)  # Alteração 1: Calcular a soma inicial do lado esquerdo
+somaDireita = sum(direita)    # Alteração 2: Calcular a soma inicial do lado direito
 
 while True:
 
-
+    # Se o lado direito e o esquerdo estiverem com o msm peso, encerramos a execução
     if somaEsquerda == somaDireita:
         
         # anilhas = l + d- (r+1) 
@@ -23,13 +24,13 @@ while True:
         if somaEsquerda >somaDireita :  # Alteração 3: Correção da condição
             somaEsquerda -= esquerda[l]
             l += 1
-            anilhas +=1
+            anilhasRetiradas +=1
             
         else:
    
             
             somaDireita -= direita[r]
-            anilhas+=1 
+            anilhasRetiradas+=1 
             
             r -= 1
         #else:
